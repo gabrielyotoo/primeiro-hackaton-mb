f519350909d6fdb0232d85d2378c7a7bbebb65e1
Gabriel Yamamoto <gabrielyamamoto82@gmail.com>
2021-03-13 15:01:37 -0300

    feat(mobile): starrt targe details screen

--- packages/mobile/src/screens/Home/homeNavigator.js
+++ packages/mobile/src/screens/Home/homeNavigator.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Header from '../../components/Header';
+
 const HomeNavigator = createStackNavigator();
 
 const Home = () => (
-  <HomeNavigator.Navigator>
+  <HomeNavigator.Navigator initialRouteName="targetDetail">
     <HomeNavigator.Screen name="home" component={Home} />
-    <HomeNavigator.Screen name="targetDetail" component={Home} />
+    <HomeNavigator.Screen
+      name="targetDetail"
+      component={Home}
+      options={{
+        header: (props) => <Header {...props} title="meta" />,
+      }}
+    />
     <HomeNavigator.Screen name="goalDetail" component={Home} />
   </HomeNavigator.Navigator>
 );
